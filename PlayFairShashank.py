344a982171bea34f4b7e8f4803552cae3a66e8b4
Spongebob <shashank.rocks29@gmail.com>
2019-02-02 14:01:30 +0530

    Encryption complete

--- PlayFairShashank.py
+++ PlayFairShashank.py
@@ -21,10 +21,9 @@ def start():
             if (char not in key and (char!='j')):
                 key = key + char
         else:
-            if (char not in key and (char!='j')):
+            if (char not in key and (char!='q')):
                 key = key + char
-    #print(len(key))
-    #print(key)
+
     m=0
     for i in range(5):
         for j in range(5):
@@ -32,13 +31,13 @@ def start():
             m = m+1
     for row in matrix:
         keymat.extend(row)
-    #print(keymat)
+    print("\nKey Matrix: ")
     for i in range(0, 5):
         print(matrix[i])
     
 
 def genKey():
-    lst = input("Enter Plain Text: ").split()
+    lst = input("\nEnter Plain Text: ").split()
     text = "".join(lst)
     
     for i in range(1, len(text)):
@@ -46,17 +45,20 @@ def genKey():
             text=text[0:i]+'x'+text[i:]
             
     if len(text)%2==0:
-        print("Even Length")
-        print(len(text))
+        #print("Even Length")
+        #print(len(text))
+        pass
     else:
-        print("Odd Length")
+        #print("Odd Length")
         text=text +"z"
+    print("Modified plain text: {0}".format(text))
     return text
 
 def encrypt(a, b):
     col1, col2, row1, row2 = 0,0,0,0
     col1 = int(keymat.index(a)%5)
     col2 =  int(keymat.index(b)%5)
+    temp = ""
     #print("Col ",col1, col2)
 
     row1 = int(keymat.index(a)/5)
@@ -64,17 +66,19 @@ def encrypt(a, b):
     #print("Row ",row1, row2)
 
     if (col1==col2) and (row1!=row2):           #Same Column
-        cipher = cipher + matrix[(row1+1)%5][col1] + matrix[(row2+1)%5][col1]
+        temp = temp + matrix[(row1+1)%5][col1] + matrix[(row2+1)%5][col1]
     elif (col1!=col2) and (row1==row2):         #Same Row
-        cipher = cipher + matrix[row1][(col1+1)%5] + matrix[row1][(col2+1)%5]
+        temp = temp + matrix[row1][(col1+1)%5] + matrix[row1][(col2+1)%5]
     elif (col1!=col2) and (row1!=row2):         #Rectangle
-        cipher = cipher + matrix[row2][col1] + matrix[row1][col2]
+        temp = temp + matrix[row1][col2] + matrix[row2][col1] 
+    return temp
     
     
 def main():
     start()
     plainText = genKey()
+    cipher = ""
     for i in range(1, len(plainText), 2):
-        encrypt(plainText[i-1], plainText[i])
-    print(cipher)
+        cipher = cipher + encrypt(plainText[i-1], plainText[i])
+    print("\nCipher text: {0}".format(cipher))
 main()
